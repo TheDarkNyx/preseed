efb3c82e730b30d022f58a00ae2d82e3f3de1b4a
UnholyDawns <RoyPaz@OutLook.com>
2016-10-15 01:06:13 +0300

    Add files via upload

---
+++ New-Linux.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+cat <<EOT >> /etc/sysctl.conf
+net.ipv4.icmp_echo_ignore_broadcasts = 1
+net.ipv4.icmp_echo_ignore_all = 1
+net.ipv4.ip_default_ttl = 128
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
+EOT
+apt-get install -y linux-headers-$(uname -r);apt-get install -y gcc;apt-get install -y make;apt-get install -y unattended-upgrades
