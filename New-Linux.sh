7228563a6f47f5ed3c30e21e23a2134bc7f96e25
UnholyDawns <RoyPaz@OutLook.com>
2016-10-15 05:05:34 +0300

    Update New-Linux.sh

--- New-Linux.sh
+++ New-Linux.sh
@@ -7,4 +7,5 @@ net.ipv6.conf.all.disable_ipv6 = 1
 net.ipv6.conf.default.disable_ipv6 = 1
 net.ipv6.conf.lo.disable_ipv6 = 1
 EOT
+grep -v 'deb cd' /etc/apt/sources.list > /etc/apt/sources1.list; mv /etc/apt/sources1.list /etc/apt/sources.list
 apt-get install -y linux-headers-$(uname -r) gcc make unattended-upgrades
